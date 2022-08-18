d4df430b2158ce62d5f94e240260d16528ba7a9b
Emmy <emmyfarrell18@gmail.com>
2022-08-17 22:47:00 -0500

    adding input

--- ao3script.py
+++ ao3script.py
@@ -5,15 +5,17 @@ import csv
 
 from ao3 import utils
 header = ['Title', 'Author', 'Fandoms', 'Relationships', 'Summary', 'Characters', 'Additional Tags', 'Rating', 'Warnings' 'Work Url']
-url = 'https://archiveofourown.org/works/37971826'
-workid = utils.work_id_from_url(url)
-from ao3 import AO3
-api = AO3()
-work = api.work(id=workid)
-metadata = [work.title, work.author, work.fandoms, work.relationship, work.summary, work.characters, work.additional_tags, work.rating, work.warnings, work.url]
-file = open('fanficlist.csv', 'a')
-writer = csv.writer(file)
-writer.writerow(header)
-writer.writerow(metadata)
-#time.sleep(5)
-file.close()
+urlInput = str(input())
+urlList = urlInput.split()
+for url in urlList:
+    workid = utils.work_id_from_url(url)
+    from ao3 import AO3
+    api = AO3()
+    work = api.work(id=workid)
+    metadata = [work.title, work.author, work.fandoms, work.relationship, work.summary, work.characters, work.additional_tags, work.rating, work.warnings, work.url]
+    file = open('fanficlist.csv', 'a')
+    writer = csv.writer(file)
+    #writer.writerow(header)
+    writer.writerow(metadata)
+    file.close()
+    time.sleep(5)
